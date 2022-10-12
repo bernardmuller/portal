3ed513f64451b8d77cf78a1a7213eba19ae0f1f1
Bernard <b.mullerjnr@gmail.com>
2022-10-12 12:15:40 +0200

    api proxy

--- index.ts
+++ index.ts
@@ -2,12 +2,14 @@ import express from "express";
 import httpProxy from "express-http-proxy";
 const app = express();
 var serverOne = "https://munchiesbackend-production.up.railway.app/";
+var serverOneAPI = "https://munchiesbackend-production.up.railway.app/api";
 
 app.get("/", function (req, res) {
 	res.send("bernardmuller.dev api portal");
 });
 
-app.use("/munchies/*", httpProxy(serverOne));
+app.use("/munchies", httpProxy(serverOne));
+app.use("/munchies/api", httpProxy(serverOneAPI));
 
 const port = 8080;
 
