cceffc3e8e294c1759c24781b52a23c02b828daa
Bernard <b.mullerjnr@gmail.com>
2022-10-12 16:14:54 +0200

    feat: basic proxy based on services

--- index.ts
+++ index.ts
@@ -1,17 +1,61 @@
-import express from "express";
+import express, { Application } from "express";
 import httpProxy from "express-http-proxy";
+import dotenv from "dotenv";
+dotenv.config();
+
 const app = express();
-var serverOne = "https://munchiesbackend-production.up.railway.app/";
-var serverOneAPI = "https://munchiesbackend-production.up.railway.app/api";
+
+type TService = {
+	name: string;
+	developmentUrl: string;
+	productionUrl: string;
+	queryParam: string;
+};
+
+const services: TService[] = [
+	{
+		name: "munchies",
+		developmentUrl: `http://localhost:5000/api`,
+		productionUrl: "https://munchiesbackend-production.up.railway.app/api",
+		queryParam: "/munchies",
+	},
+];
+
+function requireEnvVar(key: string) {
+	const variable = process.env[key];
+	if (!variable)
+		throw new Error(`environment variable with name "${key}" not found`);
+	return variable;
+}
+
+function checkEnvironmentIsProduction() {
+	return requireEnvVar("NODE_ENV") === "production";
+}
+
+function buildProxyUrl(service: TService) {
+	const url = checkEnvironmentIsProduction()
+		? service.productionUrl
+		: service.developmentUrl;
+	console.log(url);
+	return url;
+}
+
+function createEndpoint(app: Application, service: TService) {
+	app.use(service.queryParam.toString(), httpProxy(buildProxyUrl(service)));
+}
+
+function createProxyEndpoints(services: TService[]) {
+	services.forEach((service) => createEndpoint(app, service));
+}
+
+createProxyEndpoints(services);
 
 app.get("/", function (req, res) {
 	res.send("bernardmuller.dev api portal");
 });
 
-app.use("/munchies", httpProxy(serverOne));
-app.use("/munchies/api", httpProxy(serverOneAPI));
-
-const port = 8080;
+const port =
+	requireEnvVar("PORT") === "production" ? requireEnvVar("PORT") : 6000;
 
 app.listen(port, () => {
 	console.log("Server listening on port " + port);
